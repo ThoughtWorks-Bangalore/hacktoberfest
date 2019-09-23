f3f2647fbbbfe02e110c57d7b8c06803ee92bab8
Saurabh M. Machave <unknown7bolt@gmail.com>
2019-09-23 18:41:57 +0530

    add geeknight link to navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,36 +3,45 @@ import { Link } from 'gatsby'
 import Scrollspy from 'react-scrollspy'
 import Scroll from './Scroll'
 
-const Nav = (props) => (
-    <nav id="nav" className={props.sticky ? 'alt' : ''}>
-        <Scrollspy items={ ['intro', 'first', 'cta'] } currentClassName="is-active" offset={-300}>
-            <li>
-                <Scroll type="id" element="intro">
-                    <a href="/#">Introduction</a>
-                </Scroll>
-            </li>
-            <li>
-                <Scroll type="id" element="first">
-                    <a href="/#">More</a>
-                </Scroll>
-            </li>
-            <li>
-                <Scroll type="id" element="cta">
-                    <a href="/#">Location</a>
-                </Scroll>
-            </li>
-            <li>
-                <Link to="/agenda">
-                    Agenda
-                </Link>
-            </li>
-            <li>
-                <Link to="/other-offices">
-                    Other offices
-                </Link>
-            </li>
-        </Scrollspy>
-    </nav>
+const Nav = props => (
+  <nav id="nav" className={props.sticky ? 'alt' : ''}>
+    <Scrollspy
+      items={['intro', 'first', 'cta']}
+      currentClassName="is-active"
+      offset={-300}
+    >
+      <li>
+        <Scroll type="id" element="intro">
+          <a href="/#">Introduction</a>
+        </Scroll>
+      </li>
+      <li>
+        <Scroll type="id" element="first">
+          <a href="/#">More</a>
+        </Scroll>
+      </li>
+      <li>
+        <Scroll type="id" element="cta">
+          <a href="/#">Location</a>
+        </Scroll>
+      </li>
+      <li>
+        <Link to="/agenda">Agenda</Link>
+      </li>
+      <li>
+        <Link to="/other-offices">Other offices</Link>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="http://thoughtworks-bangalore.github.io/geeknight"
+        >
+          Geeknight
+        </a>
+      </li>
+    </Scrollspy>
+  </nav>
 )
 
 export default Nav
